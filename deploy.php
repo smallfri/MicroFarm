f2b6b7ab0969c073bf9e569ae15cdb5c836b2e48
smallfri <russell@smallfri.com>
2018-11-28 13:21:29 -0500

    INIT

---
+++ deploy.php
@@ -0,0 +1,41 @@
+<?php
+namespace Deployer;
+
+require 'recipe/laravel.php';
+
+// Project name
+set('application', 'microfarmmanager');
+
+// Project repository
+set('repository', 'git@domain.com:smallfri/microfarmmanager.git');
+
+// [Optional] Allocate tty for git clone. Default value is false.
+set('git_tty', true); 
+
+// Shared files/dirs between deploys 
+add('shared_files', []);
+add('shared_dirs', []);
+
+// Writable dirs by web server 
+add('writable_dirs', []);
+
+
+// Hosts
+
+host('app.microfarmmanager.com')
+    ->user('appmicro')
+    ->set('deploy_path', '~/public_html');
+    
+// Tasks
+
+task('build', function () {
+    run('cd {{release_path}} && build');
+});
+
+// [Optional] if deploy fails automatically unlock.
+after('deploy:failed', 'deploy:unlock');
+
+// Migrate database before symlink new release.
+
+before('deploy:symlink', 'artisan:migrate');
+
