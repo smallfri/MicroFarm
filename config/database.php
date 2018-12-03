bc9f5bc5af103d717aab89ad70cf7a023a402da3
Ubuntu <ubuntu@ip-172-31-7-45.ec2.internal>
2018-12-03 18:03:15 +0000

    from server

--- config/database.php
+++ config/database.php
@@ -42,11 +42,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => 'farm113002.cluster-cjsxndgdnfzl.us-east-1.rds.amazonaws.com',
-            'port' => 3306,
-            'database' => 'appmicro_db',
-            'username' => 'farm113002',
-            'password' => 'farm113002',
+            'host' =>  env('DB_HOST', 'mysql'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE'),
+            'username' => env('DB_USERNAME'),
+            'password' => env('DB_PASSWORD'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
